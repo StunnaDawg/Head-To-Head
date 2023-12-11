309c6260b21978b4c238e66d4f0fdc8fe7cd58bd
StunnaDawg <jonsonallen9@gmail.com>
2023-12-11 12:36:05 -0400

    server set up

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,21 @@
 import React from "react"
+import useSWR from "swr"
 import "./index.css"
 
+export const ENDPOINT = "http://localhost:4000"
+
+const fetcher = (url: string) =>
+  fetch(`${ENDPOINT}/${url}`).then((r) => r.json())
+
 function App() {
+  const { data, mutate } = useSWR("dashboard", fetcher)
+
   return (
     <>
       <div className="flex-1 flex flex-row justify-center">
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <h1 className="text-3xl font-bold underline">
+          data:{JSON.stringify(data)}
+        </h1>
       </div>
     </>
   )
--- server/main.go
+++ server/main.go
@@ -1,53 +1,51 @@
 package main
 
 import (
-	"context"
-	"fmt"
 	"log"
 	"github.com/gofiber/fiber/v2"
-	"github.com/redis/go-redis/v9"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
+
+
 
 func main() {
 
 	app := fiber.New()
 
-	app.Get("/healthcheck", func(c * fiber.Ctx) error {
-		return c.SendString("ok")
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "http://localhost:5173",
+		AllowHeaders: "Origin, Content-Type, Accept",
+	}))
+
+	app.Get("/dashboard", func(c * fiber.Ctx) error {
+		return c.SendString("dashboard")
+	})
+
+	app.Get("/login", func(c * fiber.Ctx) error {
+		return c.SendString("login")
+	})
+
+	app.Get("/signup", func(c * fiber.Ctx) error {
+		return c.SendString("signup")
+	})
+
+	app.Post("/login", func(c * fiber.Ctx) error {
+		return c.SendString("login post")
+	})
+
+	app.Post("/signup", func(c * fiber.Ctx) error {
+		return c.SendString("signup post")
+	})
+
+
+	app.Get("/player-list", func(c * fiber.Ctx) error {
+		return c.SendString("player list")
+	})
+
+	app.Get("/head-to-head", func(c * fiber.Ctx) error {
+		return c.SendString("head to head")
 	})
 
 	log.Fatal(app.Listen(":4000"))
-
-
-	client := redis.NewClient(&redis.Options{
-        Addr:	  "localhost:6379",
-        Password: "", // no password set
-        DB:		  0,  // use default DB
-    })
-
-	ctx := context.Background()
-
-err := client.Set(ctx, "foo", "bar", 0).Err()
-if err != nil {
-    panic(err)
-}
-
-val, err := client.Get(ctx, "foo").Result()
-if err != nil {
-    panic(err)
-}
-fmt.Println("foo", val)
 	
-
-	session := map[string]string{"name": "John", "surname": "Smith", "company": "Redis", "age": "29"}
-for k, v := range session {
-    err := client.HSet(ctx, "user-session:123", k, v).Err()
-    if err != nil {
-        panic(err)
-    }
-}
-
-userSession := client.HGetAll(ctx, "user-session:123").Val()
-fmt.Println(userSession)
-
 }
